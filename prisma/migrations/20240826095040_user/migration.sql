ed0ccb3c173fb26f343487fcce35b71bae53f090
iamsyygo <2683030687@qq.com>
2024-08-26 23:14:17 +0800

    🔧 创建用户表并添加注释
    - 创建用户表，并添加了注释说明每个字段的用途和含义
    - 更新了用户表的字符集和排序规则

--- prisma/migrations/20240826095040_user/migration.sql
+++ prisma/migrations/20240826095040_user/migration.sql
@@ -1,11 +1,12 @@
--- CreateTable
+-- 创建用户表
 CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(50) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT '用户ID，自增',
+    `username` VARCHAR(191) NOT NULL COMMENT '用户名',
+    `password` VARCHAR(50) NOT NULL COMMENT '用户密码',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
+    `updatedAt` DATETIME(3) NOT NULL COMMENT '更新时间',
+
 
     UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT='用户表';
