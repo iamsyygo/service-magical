0fa5454433cfe3bbd438e8e970c96d54a2f288d8
李雨锋 <liyufeng@gz-yibo.com>
2024-08-30 17:38:43 +0800

    🔧 user module imports and dependencies

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -1,14 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { CommonModule } from '@app/common';
+import { AuthGuard } from '@app/common/guard/auth.guard';
+import { EmailModule } from '@app/email';
 import { PrismaModule } from '@app/prisma';
 import { RedisModule } from '@app/redis';
-import { EmailModule } from '@app/email';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '@app/common/guard/auth.guard';
-import { CommonModule } from '@app/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
@@ -17,20 +16,6 @@ import { CommonModule } from '@app/common';
     RedisModule,
     EmailModule,
     CommonModule,
-    // JwtModule.registerAsync({
-    //   inject: [ConfigService],
-    //   imports: [ConfigModule],
-    //   global: true,
-    //   useFactory(configService: ConfigService) {
-    //     const secret = configService.get('JWT_ACCESS_SECRET');
-    //     return {
-    //       secret,
-    //       signOptions: {
-    //         expiresIn: '30m', // 默认 30 分钟
-    //       },
-    //     };
-    //   },
-    // }),
   ],
   controllers: [UserController],
   providers: [UserService, { provide: APP_GUARD, useClass: AuthGuard }],
